763e3329e1f09d3e189da21cc359e0b7ec85083b
Peter Spada <spadapet@hotmail.com>
2024-07-31 14:37:07 -0700

    Add HourlyMaui blank project

---
+++ HourlyMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HourlyMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ HourlyMaui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HourlyMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
